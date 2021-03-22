053255962cbf1b2d6a3d57ee103d1ead7e4e7349
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-03-22 13:18:30 +0900

    second commit

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import TodoList from './components/TodoList';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import TodoInsert from './components/TodoInsert';
+
+const App = () => {
+  // todos: {id: Number, textValue: string, checked: boolean }
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = text => {
+    setTodos([
+      ...todos,
+      {id: Math.random().toString(), textValue: text, checked: false},
+    ]);
+  };
+
+  const onRemove = id => e => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const onToggle = id => e => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? {...todo, checked: !todo.checked} : todo,
+      ),
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>T o d o !</Text>
+      <View style={styles.card}>
+        <TodoInsert onAddTodo={addTodo} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '500',
+    textAlign: 'center',
+    backgroundColor: 'black',
+  },
+  card: {
+    backgroundColor: '#2E2E2E',
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
+
+export default App;
